79df5800e3c7434ef23b00ab32455a99e285c7fe
Tiago Moreira <tiago10moreira1@gmail.com>
2021-06-24 09:21:57 -0300

    feat: add payment endpoints

---
+++ php/www/src/payment/dtos/payment.dtos.php
@@ -0,0 +1,44 @@
+<?php
+
+  class GetPaymentOptionDto {
+    public function __construct(
+      public int $id,
+      public string $name,
+      public string $paymentMethod,
+      public bool $default,
+      public ?GetCreditCardDto $payment
+    ) {}
+  }
+
+  class GetCreditCardDto {
+    public function __construct(
+      public string $owner_name,
+      public string $last_digits,
+      public string $due_date,
+    ) {}
+  }
+
+  class CreatePaymentDto {
+    public function __construct(
+      public string $name,
+      public string $paymentMethod,
+      public string $owner_name,
+      public string $card_number,
+      public string $due_date,
+      public string $ccv
+    ) {}
+  }
+
+  class GetPaymentDto {
+    public function __construct(
+      public int $id,
+      public string $name,
+      public string $paymentMethod,
+      public bool $default,
+      public string $owner_name,
+      public string $last_digits,
+      public string $due_date,
+    ) {}
+  }
+
+?>
--- php/www/src/payment/services/payment.service.php
+++ php/www/src/payment/services/payment.service.php
@@ -1,7 +1,48 @@
 <?php
+  require_once (__DIR__ . '/../repositories/payment.repository.php');
+  require_once (__DIR__ . '/../repositories/credit-card.repository.php');
 
   class PaymentService {
+      public static function create(int $userId, CreatePaymentDto $createPaymentDto): GetPaymentOptionDto {      
+      $paymentOption = PaymentRepository::create($userId, $createPaymentDto);
+      $credit_card = CreditCardRepository::create($userId, $createPaymentDto);
+
+      $paymentOption->payment = $credit_card;
+      return $paymentOption;
+    }
+
+    public static function list(int $userId): array {
+
+      return PaymentRepository::list($userId);
+    }
+
+    public static function read (int $paymentId): GetPaymentDto|null {
+
+      return PaymentRepository::read($paymentId);
+    }
     
+    public static function makeDefault(int $paymentId): ResponseMessage {
+      $message = new ResponseMessage("success", 200);
+      $rowsAffected = PaymentRepository::makeDefault($paymentId);
+
+      if ($rowsAffected != 1) {
+        $message = new ResponseMessage("error", 500);
+      }
+
+      return $message;
+    }
+
+    public static function delete(int $paymentId): ResponseMessage {
+      $message = new ResponseMessage("success", 200);
+      $rowsAffected = PaymentRepository::delete($paymentId);
+
+      if ($rowsAffected != 1) {
+        $message = new ResponseMessage("error", 500);
+      }
+
+      return $message;
+    }
+   
   }
 
 ?>
